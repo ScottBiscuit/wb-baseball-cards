6a8227a296cf7d1d29ab4b5a23c0b634a9275d47
Scott Johnstone <scottajohnstone@gmail.com>
2024-06-24 16:00:55 -0500

    added child components and added props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import playerData from "./playerData.js";
+
+
+
 function BaseballCard(props) {
   return (
     <div className="card">
-      <h2>Player Name goes here</h2>
+      <h2>{props.name}</h2>
+      <img src={props.imgUrl} alt={props.name} />
     </div>
   );
 }
 
 function App() {
-  return <BaseballCard />;
+  const cards = playerData.map((player) => ( <BaseballCard 
+    name = {player.name}
+    team = {player.team}
+    position = {player.position}
+    stats = {player.stats}
+    imgUrl = {player.imgUrl}
+    cardId = {player.cardId}
+  />))
+  return <>{cards}</>;
 }
 
 export default App;
